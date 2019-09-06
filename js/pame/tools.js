d82d4b09f8e631fb8393574b14db36bcacd71e74
AgentJay <james_e_davy@hotmial.com>
2019-09-06 17:51:27 +0200

    adding PAME views pages and relative templates

---
+++ js/pame/tools.js
@@ -0,0 +1,10 @@
+// JS PAME tools
+
+jQuery(document).ready(function($) {
+//==============================================
+
+ console.log('tools js lib loaded.');
+
+
+//==============================================
+});
