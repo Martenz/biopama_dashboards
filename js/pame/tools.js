73bebfdb0de361f1a95998fafd86736c8647920d
AgentJay <james_e_davy@hotmial.com>
2019-09-10 11:14:14 +0200

    Adding Tools Pame page template and css js

--- js/pame/tools.js
+++ js/pame/tools.js
@@ -5,5 +5,10 @@ jQuery(document).ready(function($) {
 
  console.log('tools js lib loaded.');
 
+ $('.assessment-tools-more').on('click',function(){
+   $(this).closest('.card').find('.assessment-tools-details').fadeToggle('slow');
+ });
+
+
 //==============================================
 });
